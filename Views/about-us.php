acd4536540a6052a8f6a8d59df049b80fc4e3562
unknown <skipperstrange@gmail.com>
2022-11-06 20:09:18 +0100

    Check point

--- Views/about-us.php
+++ Views/about-us.php
@@ -6,9 +6,13 @@ include_once PARTIALS.'sub-header.php';
 <div class="container">
     <div class="row pt-5 pb-5">
         <div class="row justify-content-center">
-            <div class="col-lg-8 pt-4 mt-3">
+            <div class="col-lg-8">
                 <div class="testimonial testimonial-style-2 testimonial-with-quotes mb-0">
-                    <div class="testimonial-quote">“</div>
+                    <div class="d-flex justify-content-center mb-4">
+                        <div class="profile-image-inner-container">
+                            <img class="img-fluid rounded mb-4 lazyload" data-src="<?=IMAGES_URL?>article-small.jpg" src="<?=IMAGES_URL?>article-small.jpg" />
+                        </div>
+                    </div>
                     <blockquote>
                         <p class="font-weight-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit, leo vitae interdum pretium, tortor risus dapibus tortor, eu suscipit orci leo sed nisl. Integer et ipsum eu nulla auctor mattis sit amet in diam. Vestibulum non.</p>
                     </blockquote>
@@ -23,17 +27,60 @@ include_once PARTIALS.'sub-header.php';
     </div>
 </div>
 
-<div class="container">
+<section class="container">
     <div class="row">
         <div class="row text-center">
             <div class="col-lg-8 offset-lg-2">
                 <h2 class="mb-0 text-color-dark">Best Service For Your Future Leaders</h2>
-                <p class="lead">Our goal is to ensure that all students are nurtured emotionally, academically, physically, and spiritually</p>
-                <p class=" text-justify">
+                <p>
+                <span class="lead">Our goal is to ensure that all students are nurtured emotionally, academically, physically, and spiritually</span>
+                <span class=" text-justify">
                 by nurturing their natural love of learning. We see education not just as the transmission of knowledge, but as a joyful lifelong experience that fosters the development of our students as a whole, unique individuals who respect and value themselves and others.
+                </span>
                 </p>
             <hr class="solid my-5">
             </div>
         </div>
     </div>
+</section>
+
+<section id="mission-vision" class="mb-4">
+
+<div class="row mt-4">
+    <div class="col-sm-4 text-center offset-lg-2">
+        <div class="feature-box feature-box-style-4 justify-content-center mt-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
+            <span class="featured-boxes featured-boxes-style-4 p-0 m-0">
+                <span class="featured-box featured-box-primary featured-box-effect-6 p-0 m-0">
+                    <span class="box-content p-0 m-0">
+                        <i class="icon-featured icon-target icons"></i>
+                    </span>
+                </span>
+            </span>	
+            <div class="feature-box-info">
+                <h2 class="mb-3 text-color-dark">Mission</h2>
+                <p class="mb-4">
+                Our goal is to ensure that all students are nurtured emotionally, academically, physically and spiritually by nurturing their natural love of learning.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4 text-center">
+        <div class="feature-box feature-box-style-4 justify-content-center mt-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
+            <span class="featured-boxes featured-boxes-style-4 p-0 m-0">
+                <span class="featured-box featured-box-primary featured-box-effect-6 p-0 m-0">
+                    <span class="box-content p-0 m-0">
+                        <i class="icon-featured icon-eye icons"></i>
+                    </span>
+                </span>
+            </span>	
+            <div class="feature-box-info">
+                <h2 class="mb-3 text-color-dark">Vision</h2>
+                <p class="mb-4">
+                we aim to inspire our students to push beyond the confines of conventional educational standards by helping them walk the path that makes us all human, the desire to learn, discover, explore, create and manifest what goes beyond our wildest imagination!
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
+
+</section>
